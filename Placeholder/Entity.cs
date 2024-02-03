7c2aca7c42c18dbac5e4f76c37c0d7549cb7767b
RicoCruzSuave <RicoCruzSuave@users.noreply.github.com>
2024-02-03 15:37:29 -0800

    Added Text Effects

--- Placeholder/Entity.cs
+++ Placeholder/Entity.cs
@@ -18,6 +18,7 @@ public partial class Entity : CharacterBody2D
     public AnimatedSprite2D animatedSprite;
     public Sprite2D Sprite; //Todo: remove it in favor of AnimatedSprite2D
     public AnimationPlayer Animation; //Todo: remove it in favor of AnimatedSprite2D
+    public PackedScene TextEffectScene = GD.Load<PackedScene>("res://Placeholder/TextEffect.tscn");
 
     public override void _Ready()
     {
@@ -69,12 +70,22 @@ public partial class Entity : CharacterBody2D
         Animation.PlayBackwards("Attack");
         await ToSignal(Animation, AnimationPlayer.SignalName.AnimationFinished);
         Target.Hit(this.CurrentAttack);
+
+        var new_text_effect = (TextEffect)TextEffectScene.Instantiate();
+        new_text_effect.Launch(new Vector2(0, -1), 1.0);
+        new_text_effect.text = "Attack";
+        AddChild(new_text_effect);
     }
 
     public void Hit(int Damage)
     {
         CurrentHP -= Damage - CurrentDefense;
         CheckForDeath();
+
+        var new_text_effect = (TextEffect)TextEffectScene.Instantiate();
+        new_text_effect.Launch(new Vector2(0, -1), 1.0);
+        new_text_effect.text = "Hit " + Damage.ToString();
+        AddChild(new_text_effect);
     }
 
     public bool CheckForDeath()
---
+++ Placeholder/TextEffect.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+public partial class TextEffect : RigidBody2D
+{
+    [Export]
+    public string text = "Test";
+    [Export]
+    public float speed = 500.0F;
+    [Export]
+    public float time = 1.5F;
+    private Timer timer;
+    private Label label;
+    public override void _Ready()
+    {
+        label = GetNode<Label>("Label");
+        timer = GetNode<Timer>("Timer");
+        label.Text = text;
+        timer.WaitTime = time;
+        timer.Start();
+
+    }
+
+    public override void _Process(double delta)
+    {
+        if (timer.IsStopped()) { Free(); }
+        Color mod = Modulate;
+        mod.A = (float)((timer.TimeLeft / timer.WaitTime) * (timer.TimeLeft / timer.WaitTime));
+        Modulate = mod;
+    }
+
+    public void Launch(Vector2 dir, double variance)
+    {
+        var rand = new Random();
+        float rand_variance = (float)(rand.NextDouble() * (variance - -variance) + -variance);
+        Vector2 impulse = dir.Rotated(rand_variance);
+        ApplyCentralImpulse(impulse * speed);
+    }
+}
+
