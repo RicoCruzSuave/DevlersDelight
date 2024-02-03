a21f26013838bbdb06db4fa2f9a5ba5949761ed2
DevManiac <fabian.schneider221@gmail.com>
2024-02-03 23:42:46 +0100

    Reworked code structure
    Rewrote messy code into more organized code.
    Added "open desk" for team manager and world map

--- Scripts/Managers/GameManager.cs
+++ Scripts/Managers/GameManager.cs
@@ -1,15 +1,31 @@
 using Godot;
 using System;
+using System.Linq;
 
-public partial class GameManager : Control
+public partial class GameManager : Node
 {
+	//Reference to Player Resource
+	[Export] private Player player;
+	//Reference to UIManager
+	[Export] UIManager ui;
+	[Export] NavigationManager navManager;
 
+	[ExportGroup("Pre Game")]
+	[Export] private LineEdit in_playerName;
 
-	//[ExportGroup("Dependencies")]
-	//--------------------------
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	//Gets called when the player clicks the "Click here to start" Button and Sets his initial Data
+	public void SetPlayerName()
 	{
+		string playerName = in_playerName.Text;
+		if(string.IsNullOrWhiteSpace(playerName))
+		{
+			ui.SetPreGameInfoLabel("Please fill in all information to continue.");
+			return;
+		} 
+
+		player.PlayerName = playerName;
+		ui.UpdatePlayerStatsUI(player);
+		navManager.SwitchToMainMenu();
+
 	}
 }
